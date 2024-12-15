c72115692ed97acd563e65498a69b21a164573c3
DivyaJyothiVundavalli <68144559+DivyaJyothiVundavalli@users.noreply.github.com>
2024-12-15 13:01:33 +0530

    Update main.tf

--- CD_EKS/main.tf
+++ CD_EKS/main.tf
@@ -52,14 +52,16 @@ module "eks" {
   }
 }
 
+# EKS Cluster Data
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
+# EKS Cluster Auth Data
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
 }
-
 
 
 # Output cluster_name
